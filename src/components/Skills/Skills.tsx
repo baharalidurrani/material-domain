85008403195ffa96ab4e457b615fa17eeebd1adb
durrani <baharalidurrani@gmail.com>
2021-04-21 18:46:51 -0400

    extra components added

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Skills() {
+  return (
+    <div>
+      <h1>Skills</h1>
+    </div>
+  );
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,9 +4,16 @@ import { LongText } from "../components/Placeholder/LongText";
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from "@material-ui/icons/Info";
 import NewReleasesIcon from "@material-ui/icons/NewReleases";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { Skills } from "../components/Skills/Skills";
+import { Interests } from "../components/Interests/Interests";
+import { Contact } from "../components/Contact/Contact";
 
 export const ROUTES = [
   { name: "Home", path: "/", component: Home, icon: HomeIcon },
+  { name: "Skills", path: "/skills", component: Skills, icon: FavoriteIcon },
+  { name: "Interests", path: "/interests", component: Interests, icon: NewReleasesIcon },
+  { name: "Contact", path: "/contact", component: Contact, icon: NewReleasesIcon },
   { name: "Long Text", path: "/long", component: LongText, icon: NewReleasesIcon },
   { name: "About", path: "/about", component: About, icon: InfoIcon },
 ];
