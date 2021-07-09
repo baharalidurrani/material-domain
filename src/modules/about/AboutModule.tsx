5b9db9cb0d95bfe1ed2bff03be19be18b576c98f
baharalidurrani <baharalidurrani@gmail.com>
2021-07-10 01:18:10 +0500

    About text added

--- src/modules/about/AboutModule.tsx
+++ src/modules/about/AboutModule.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
 
 type Props = {};
 
 export function AboutModule({}: Props): React.ReactElement {
   console.log("AboutModule Rendered");
   return (
-    <>
-      <h1>AboutModule Coming Soon...</h1>
-    </>
+    <Container>
+      <Typography variant="h1" align="center">
+        AboutModule Coming Soon...
+      </Typography>
+      <Typography variant="h6" align="center">
+        Welcome to the most Efficient, Elegant and Easy to Excel production ready React template of
+        2021 that one can <del>ever</del> never find on the web.
+      </Typography>
+    </Container>
   );
 }
