2e38fc9b8b41c26b34e74c254ef83b4bdfd6aa2a
baharalidurrani <baharalidurrani@gmail.com>
2021-07-12 11:47:57 +0500

    About typography

--- src/modules/about/AboutModule.tsx
+++ src/modules/about/AboutModule.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 
 type Props = {};
@@ -9,10 +9,12 @@ export function AboutModule({}: Props): React.ReactElement {
 
   return (
     <Container>
-      <Typography variant="h1" align="center">
-        AboutModule Coming Soon...
-      </Typography>
-      <Typography variant="h6" align="center">
+      <Box m={6}>
+        <Typography variant="h2" align="center">
+          AboutModule Coming Soon...
+        </Typography>
+      </Box>
+      <Typography variant="body1" align="center">
         Welcome to the most Efficient, Elegant and Easy to Excel production ready React template of
         2021 that one can <del>ever</del> never find on the web.
       </Typography>
