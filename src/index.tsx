023beb8b264de7e7e06c6c73ba6ffad1016e4568
baharalidurrani <baharalidurrani@gmail.com>
2022-04-10 16:39:52 +0500

    migration react v18 and packages update

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { store } from "./app-redux/store";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
