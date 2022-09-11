561f3f25969eff7ecdbc7c6b99ddb27b2ba8df73
baharalidurrani <baharalidurrani@gmail.com>
2022-09-11 13:13:49 +0500

    About Module Wrapped in PageContainer

--- src/modules/about/AboutModule.tsx
+++ src/modules/about/AboutModule.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import Container from '@mui/material/Container';
+import { PageContainer } from 'src/components/appLayout/PageContainer';
 
 export default function AboutModule(): React.ReactElement {
   console.log('AboutModule Rendered');
 
   return (
-    <Container>
+    <PageContainer>
       <Box m={6}>
-        <Typography variant="h2" align="center">
-          AboutModule Coming Soon...
+        <Box m={3}>
+          <Typography variant="h2" align="center">
+            AboutModule Coming Soon...
+          </Typography>
+        </Box>
+        <Typography variant="body1" align="center">
+          Welcome to the most Efficient, Elegant and Easy to Excel production ready React template of 2021 that one can
+          <del>ever</del> never find on the web.
         </Typography>
       </Box>
-      <Typography variant="body1" align="center">
-        Welcome to the most Efficient, Elegant and Easy to Excel production ready React template of 2021 that one can{' '}
-        <del>ever</del> never find on the web.
+      <Typography variant="body2" align="center">
+        This page is loaded lazily.
       </Typography>
-    </Container>
+    </PageContainer>
   );
 }
