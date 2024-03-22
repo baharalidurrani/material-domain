f789515a9b4bd2c19c5046a2df7e0cc6ef81201f
baharalidurrani <baharalidurrani@gmail.com>
2024-03-22 23:05:51 +0500

    📲 PWA Configuration
    Progressive Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,54 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import { VitePWA, type ManifestOptions, type VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> = {
+  name: 'Bahar Ali Durrani',
+  short_name: 'Durrani',
+  description:
+    'Remote Software Engineer Bahar Ali Durrani - Web App Developer | Node | React | Typescript | JavaScript | MERN Developer',
+  theme_color: '#9c27b0',
+  background_color: '#9c27b0',
+  display: 'standalone',
+  icons: [
+    {
+      src: 'favicon/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: 'favicon/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+    {
+      src: 'favicon/maskable_icon.png',
+      sizes: '1024x1024',
+      type: 'image/png',
+      // purpose: 'any maskable',
+    },
+  ],
+};
+const pwaOptions: Partial<VitePWAOptions> = {
+  manifest,
+  // injectRegister: 'auto',
+  registerType: 'autoUpdate',
+  // devOptions: {
+  //   enabled: true,
+  // },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,}'],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://baharalidurrani.github.io/material-domain",
-  plugins: [react(), basicSsl()],
+  // base: 'https://localhost:5173', // Dev
+  // base: 'https://localhost:4173', // Build
+  base: 'https://durrani.dev', // Prod
+  // build: { sourcemap: false },
+  plugins: [react(), mkcert(), VitePWA(pwaOptions)],
   // optimizeDeps: {
   //   include: [
   //     "@mui/material/Tooltip",
@@ -15,7 +58,7 @@ export default defineConfig({
   // },
   resolve: {
     alias: {
-      src: "/src",
+      src: '/src',
     },
   },
 });
